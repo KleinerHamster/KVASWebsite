07befbbfbacf83766484007bbbedaf30c5a3918a
Victoria <vika.ivashova.04@mail.ru>
2022-03-30 21:25:47 +0300

    vi commit lab7_8 with pdb

--- WebsiteKVAS/myform.py
+++ WebsiteKVAS/myform.py
@@ -1,6 +1,12 @@
 from bottle import post, request
+import pdb
 
 @post('/homepage', method='post')
 def my_form():
+    questions={}
     mail = request.forms.get('email')
+    email= mail
+    question= request.forms.get('QUEST')
+    questions[email]=question
+    pdb.set_trace()
     return "Thanks! The answer will be sent to the mail %s" % mail
