5b169e4b7824338f64a9932ab06b1c1ac088a224
Victoria <vika.ivashova.04@mail.ru>
2022-05-23 19:04:47 +0300

    Update comments

--- WebsiteKVAS/unitests/vi/test_checkPhone_v.py
+++ WebsiteKVAS/unitests/vi/test_checkPhone_v.py
@@ -2,9 +2,9 @@ import unittest
 import re
 
 def check_phone(phone):#finction of check phone
-    compil=re.compile(r'(\+7)(\([0-9]{3}\)\s([0-9]{3}))(-[0-9]{2})(-[0-9]{2})$', re.X)#создаем регулярное выражение
-    result=compil.search(phone)#проверяем введенную почту
-    if result:#если почта верная
+    compil=re.compile(r'(\+7)(\([0-9]{3}\)\s([0-9]{3}))(-[0-9]{2})(-[0-9]{2})$', re.X)
+    result=compil.search(phone)
+    if result:
         return 1
     else:
         return 0
